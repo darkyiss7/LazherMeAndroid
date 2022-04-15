8a6deb8186429d21ff1f74d65fa7ae388431723e
Théo Dubois <58741891+darkyiss7@users.noreply.github.com>
2022-04-15 02:32:20 +0200

    e

---
+++ app/src/main/java/fr/isen/lazherme/BleAdapter.kt
@@ -0,0 +1,94 @@
+package fr.isen.lazherme
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.le.ScanResult
+import android.content.res.ColorStateList
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import fr.isen.lazherme.R
+
+
+class BleAdapter(private val bleliste: ArrayList<ScanResult>,val clickListener : (BluetoothDevice) -> (Unit)) : RecyclerView.Adapter<BleAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textNom : TextView = itemView.findViewById(R.id.nomTextView)
+        val textAddresse : TextView = itemView.findViewById(R.id.addresseTextView)
+        val rssi : FloatingActionButton = itemView.findViewById(R.id.rssiView)
+
+    }
+
+
+
+    @SuppressLint("MissingPermission")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = bleliste[position]
+        if (item.device.name!=null){
+            holder.textNom.text = item.device.name
+        }else{
+            holder.textNom.text = "Nom inconnu"
+        }
+        holder.textAddresse.text = item.device.address
+        holder.rssi.setImageBitmap(textAsBitmap(item.rssi.toString(),20f, Color.WHITE))
+        holder.rssi.setBackgroundTintList(
+            ColorStateList.valueOf(Color
+                .parseColor(pickColor(item.rssi))));
+        holder.itemView.setOnClickListener{
+            clickListener(item.device)
+        }
+
+    }
+
+    fun pickColor(rssi : Int):String{
+        var color: String
+        when{
+            rssi<(-100) -> color= "#004361"
+            rssi<(-75) -> color= "#005f8a"
+            rssi<(-50) -> color= "#006b9c"
+            rssi<(-25) -> color= "#0083bf"
+            rssi<(-10) -> color= "#0091d4"
+            rssi<(-5) -> color= "#009be3"
+            else -> color= "#03A9F4"
+        }
+        return color
+    }
+    fun addToList(res:ScanResult){
+        val index:Int = bleliste.indexOfFirst{ it.device.address==res.device.address }
+        if(index == -1){
+            bleliste.add(res)
+        }else{
+            bleliste[index]=res
+            notifyItemInserted(bleliste.size - 1)
+        }
+        bleliste.sortBy { kotlin.math.abs(it.rssi) }
+    }
+    fun textAsBitmap(text: String?, textSize: Float, textColor: Int): Bitmap? {
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.textSize = textSize
+        paint.color = textColor
+        paint.textAlign = Paint.Align.LEFT
+        val baseline = -paint.ascent() // ascent() is negative
+        val width = (paint.measureText(text) + 0.0f).toInt() // round
+        val height = (baseline + paint.descent() + 0.0f).toInt()
+        val image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(image)
+        canvas.drawText(text!!, 0f, baseline, paint)
+        return image
+    }
+
+    override fun getItemCount(): Int {
+        return bleliste.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val View = LayoutInflater.from(parent.context).inflate(R.layout.list_items_ble, parent, false)
+        return ViewHolder(View)
+    }
+}
--- app/src/main/java/fr/isen/lazherme/MainActivity.kt
+++ app/src/main/java/fr/isen/lazherme/MainActivity.kt
@@ -1,11 +1,171 @@
 package fr.isen.lazherme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import fr.isen.lazherme.R
+import fr.isen.lazherme.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    lateinit var swipeContainer: SwipeRefreshLayout
+    private var isScanning = false
+    private val listeBle = ArrayList<ScanResult>()
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bleScanList.layoutManager = LinearLayoutManager(this)
+        binding.bleScanList.adapter = BleAdapter(listeBle){
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra(ITEM_KEY, it)
+            startActivity(intent)
+        }
+        title = "Bluetooth";
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        swipeContainer = findViewById(R.id.swipeContainer)
+        when{
+            bluetoothAdapter?.isEnabled == true ->{
+                swipeContainer.setOnRefreshListener {
+                    onRefresh()
+                }
+            }
+
+            bluetoothAdapter != null ->
+                askBluetoothPermission()
+            else -> {
+                displayBLEUnAvailable()
+            }
+        }
+        binding.bleScanImg.setOnClickListener {
+            startLeScanBLEWithPermission(!isScanning)
+        }
+        binding.bleScanText.setOnClickListener {
+            startLeScanBLEWithPermission(!isScanning)
+        }
+    }
+    fun onRefresh(){
+        startLeScanBLEWithPermission(true)
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            if (swipeContainer.isRefreshing()) {
+                swipeContainer.setRefreshing(false)
+                startLeScanBLEWithPermission(false)
+            }
+        }, 3000)
+    }
+    override fun onStop() {
+        super.onStop()
+        startLeScanBLEWithPermission(false)
+    }
+    private fun startLeScanBLEWithPermission(enable : Boolean){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )==PackageManager.PERMISSION_GRANTED
+        ){
+            startLeScanBLE(enable)
+        }else{
+            ActivityCompat.requestPermissions(this, getAllPermissions(), ALL_PERMISSION_REQUEST_CODE)
+        }
+    }
+    @SuppressLint("MissingPermission")
+    private fun startLeScanBLE(enable : Boolean) {
+        bluetoothAdapter?.bluetoothLeScanner?.apply {
+            if (enable){
+                isScanning = true
+                startScan(scanCallback)
+            }else{
+                isScanning = false
+                stopScan(scanCallback)
+            }
+            handlePlayPause()
+        }
+    }
+    private fun checkAllPermissionGranted():Boolean{
+        return getAllPermissions().all{ permission ->
+            ActivityCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+    private fun getAllPermissions(): Array<String> {
+        return if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.S){
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            )
+        }else{
+            arrayOf(
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+    private fun displayBLEUnAvailable() {
+        binding.bleScanImg.isVisible = false
+        binding.bleScanText.text=getString(R.string.ble_scan_error)
+        binding.bleScanProgression.isVisible = false
+    }
+    private fun askBluetoothPermission(){
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED
+        ){
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
+        }
+    }
+    private val scanCallback = object : ScanCallback() {
+        @SuppressLint("MissingPermission")
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            Log.d("BLEScanActivity","result : ${result.device.address}, rssi : ${result.rssi},nom : ${result.device.name}")
+            (binding.bleScanList.adapter as BleAdapter).apply {
+                addToList(result)
+                notifyDataSetChanged()
+            }
+        }
+    }
+    private fun handlePlayPause(){
+        if (isScanning){
+            binding.bleScanImg.setImageResource(R.drawable.ic_baseline_pause_24)
+            binding.bleScanText.text=getString(R.string.ble_scan_pause)
+            binding.bleScanProgression.isIndeterminate = true
+        }else{
+            binding.bleScanImg.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            binding.bleScanText.text=getString(R.string.ble_scan_play)
+            binding.bleScanProgression.isIndeterminate = false
+        }
+    }
+    companion object {
+        val DEVICE_KEY ="device_key"
+        val ITEM_KEY ="item_key"
+        private const val ALL_PERMISSION_REQUEST_CODE = 1
+        private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+
     }
 }
